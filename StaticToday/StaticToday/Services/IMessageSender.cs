ade2d438b0a182461e40f302a4e2a90e7b8fe446
Vlad Narmaniia <48920629+Leadofpride@users.noreply.github.com>
2019-04-01 22:14:38 +0300

    Add files via upload

---
+++ StaticToday/StaticToday/Services/IMessageSender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaticToday.Services
+{
+	public class MessageService
+	{
+		IMessageSender _sender;
+		public MessageService(IMessageSender sender)
+		{
+			_sender = sender;
+		}
+		public string SendMessage()
+		{
+			return _sender.Send(); 
+		}
+	}
+	public interface IMessageSender
+	{
+		string Send();
+	}
+	public class EmailMessageSender : IMessageSender
+	{
+		public string Send()
+		{
+			return "Message is sent by email";
+		}
+	}
+	public class SmsMessageSender : IMessageSender
+	{
+		public string Send()
+		{
+			return "Message is sent by sms";
+		}
+	}
+}
