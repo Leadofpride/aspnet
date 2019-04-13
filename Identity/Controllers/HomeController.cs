b35d33f4dcc3f7ddde7e7a21b3314918e6d22d1a
Vladyslav Narmaniia <narmania01pride@gmail.com>
2019-04-13 16:53:44 +0300

    Identity

---
+++ Identity/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Identity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Identity.Controllers
+{
+	public class HomeController : Controller
+	{
+		[Authorize]
+		public IActionResult Index()
+		{
+			return Content(User.Identity.Name);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
